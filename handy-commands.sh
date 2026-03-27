bb602296db5018b87bebc0aec1efa7222cd3b608
Kobe Wijnants <wijnantskobe28@gmail.com>
2026-03-27 16:26:52 +0000

    surreal update

--- handy-commands.sh
+++ handy-commands.sh
@@ -1,1 +1,10 @@
 chown -R www-data:www-data *
+ chown -R www-data:www-data /var/www/html
+
+#directories
+sudo find /var/www/html -type d -exec chmod 755 {} \;
+sudo find /var/www/html -type d -exec chmod 775 {} \;
+
+#Files: 644
+sudo find /var/www/html -type f -exec chmod 644 {} \;
+sudo find /var/www/html -type f -exec chmod 664 {} \;
